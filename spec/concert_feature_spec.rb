073bcab2cd97d0b3bc72a5d644396a74722bee5b
lewis <ljones140@gmail.com>
2015-11-24 11:13:30 +0000

    first commit

---
+++ spec/concert_feature_spec.rb
@@ -0,0 +1,10 @@
+
+
+describe 'concert merger' do
+
+  concert_merger = ConcertMerger.new({ concert_json: '../concerts.json' })
+
+  it 'groups concerts that have same artists and date' do
+
+  end
+end
